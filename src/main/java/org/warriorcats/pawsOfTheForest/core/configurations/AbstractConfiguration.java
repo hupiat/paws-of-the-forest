68d3a2df0e1d6f95bf205e4fe452dcb8347e7703
Hugo Piat <h.piat@outlook.fr>
2025-05-26 13:30:51 +0200

    Minor factorization

--- src/main/java/org/warriorcats/pawsOfTheForest/core/configurations/AbstractConfiguration.java
+++ src/main/java/org/warriorcats/pawsOfTheForest/core/configurations/AbstractConfiguration.java
@@ -21,16 +21,16 @@ public abstract class AbstractConfiguration {
     }
 
     protected static boolean checkForDefaultKey(String key, String defaultValue, String configFilePath) {
-        if (propertiesSource != null) {
-            if (!propertiesSource.containsKey(key)) {
-                propertiesSource.setProperty(key, defaultValue);
-                FileUtils.store(configFilePath, propertiesSource);
-                return false;
-            }
-        } else {
+        if (FileUtils.isYaml(configFilePath)) {
             if (!yamlSource.contains(key)) {
                 yamlSource.set(key, defaultValue);
                 FileUtils.store(configFilePath, yamlSource);
+                return false;
+            }
+        } else {
+            if (!propertiesSource.containsKey(key)) {
+                propertiesSource.setProperty(key, defaultValue);
+                FileUtils.store(configFilePath, propertiesSource);
                 return false;
             }
         }
@@ -41,7 +41,7 @@ public abstract class AbstractConfiguration {
         if (!checkForDefaultKey(key, defaultValue, configFilePath)) {
             return defaultValue;
         }
-        return propertiesSource != null ? propertiesSource.getProperty(key) : (String) yamlSource.get(key);
+        return !FileUtils.isYaml(configFilePath) ? propertiesSource.getProperty(key) : (String) yamlSource.get(key);
     }
 
     protected static ChatColor getPropertyOrDefault(String key, ChatColor defaultValue, String configFilePath) {
@@ -49,7 +49,7 @@ public abstract class AbstractConfiguration {
         if (!checkForDefaultKey(key, value, configFilePath)) {
             return defaultValue;
         }
-        return ChatColor.valueOf(propertiesSource != null ?
+        return ChatColor.valueOf(!FileUtils.isYaml(configFilePath) ?
                 propertiesSource.getProperty(key).toUpperCase() :
                 ((String) yamlSource.get(key)).toUpperCase());
     }
