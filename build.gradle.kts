3ea96f9836fb677f95795def2be7b9abd61a58cd
Hugo Piat <h.piat@outlook.fr>
2025-05-28 14:25:27 +0200

    Fixed gradle deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,22 +12,37 @@ java {
 }
 
 repositories {
-    flatDir {
-        dirs("libs")
-    }
     mavenLocal()
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.codemc.io/repository/maven-public/")
     maven("https://jitpack.io")
 }
 
 dependencies {
+    // Paper mappings
     paperDevBundle("1.20.1-R0.1-SNAPSHOT")
 
+    // Compile-only dependencies
     compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+    compileOnly("org.projectlombok:lombok:1.18.30")
+
+    // Annotation processor for Lombok
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    // ORM & SQL
     implementation("org.hibernate.orm:hibernate-core:6.4.4.Final")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    implementation("jakarta.transaction:jakarta.transaction-api:2.0.1")
+
+    // JAXB
     implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
     implementation("org.glassfish.jaxb:jaxb-runtime:4.0.2")
+
+    // Logging
+    implementation("org.jboss.logging:jboss-logging:3.5.3.Final")
+
+    // MySQL
     implementation("com.mysql:mysql-connector-j:8.4.0")
 }
 
