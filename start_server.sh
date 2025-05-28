7c041309b76f31fabe6e79233c5380f19a309d4c
Hugo Piat <h.piat@outlook.fr>
2025-05-28 19:15:28 +0200

    Updated start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./gradlew clean build
-cp build/libs/paws-of-the-forest-1.0.0-dev.jar ../Minecraft_server_paper_1.21.4/plugins/
-cd ../Minecraft_server_paper_1.21.4
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar paper-1.21.4-231.jar nogui
+cp build/libs/paws-of-the-forest-1.0.0-dev.jar ../Minecraft_server_paper_1.21.1/plugins/
+cd ../Minecraft_server_paper_1.21.1
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar paper-1.21.1-133.jar nogui
