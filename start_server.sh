6f0e22a46950ece86c01f944ce220fde2f39f5c6
Hugo Piat <h.piat@outlook.fr>
2025-05-28 14:53:55 +0200

    Fixed scripts

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn clean package
-cp target/paws-of-the-forest-1.0.0.jar ../Minecraft_server_paper_1.21.4/plugins/
+./gradlew clean build
+cp build/paws-of-the-forest-1.0.0-dev-all ../Minecraft_server_paper_1.21.4/plugins/
 cd ../Minecraft_server_paper_1.21.4
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -jar paper-1.21.4-231.jar nogui
